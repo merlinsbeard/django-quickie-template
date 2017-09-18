9a4ae1a2c6865a75866f64858afe4c78e3037fd7
Bj Paat <bjpaat01@gmail.com>
2017-09-18 13:44:59 +0800

    Fix template placeholder

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ python manage.py collectstatic --noinput
 echo Starting Gunicorn.
 exec gunicorn config.wsgi:application \
     --bind 0.0.0.0:8000 \
-    --name heyo \
+    --name {{ project_name }} \
     --workers 2 \
     --log-level=info \
 "$@"

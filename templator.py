29938e4c5e7cca83f32a43ed5b5ddd4b2569de25
Bj Paat <bjpaat01@gmail.com>
2017-09-18 14:48:43 +0800

    Update correct paths of files

--- templator.py
+++ templator.py
@@ -1,5 +1,5 @@
 from jinja2 import Template
-import os
+import os, shutil
 
 def read_file(file, **kwargs):
     with open(file, 'r') as f:
@@ -13,18 +13,27 @@ def save_file(file, data):
 
 # Create New Dockerfile
 
-project_name = input("Project Name")
+project_name = input("Project Name: ")
+cwd = os.getcwd()
 data = {"project_name": project_name}
 print("Saving File")
-save_file("Dockerfile", read_file("docker-template", **data))
+save_file(f"{cwd}/Dockerfile", read_file(f"{cwd}/docker-template", **data))
 print("Done")
 
 # Create entrypoint
+ep = os.path.join(os.getcwd(), "entrypoint.sh")
 print("Updating entrypoint")
-save_file("entrypoint.sh", read_file("entrypoint.sh", **data))
+save_file(ep, read_file(ep, **data))
 print("Done")
 
 # Rename Project Folder
 print("Rename Project Folder")
-os.rename("{{ project_name }}", data['project_name'])
+project_folder = os.path.join(os.getcwd(), "{{ project_name }}")
+os.rename(project_folder, data['project_name'])
+print("DONEEEEEEEEEE")
+
+# Delete Git
+print("Removing .git")
+git_folder = os.path.join(os.getcwd(), ".git/")
+shutil.rmtree(git_folder)
 print("DONEEEEEEEEEE")
